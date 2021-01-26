79d9202f036d263eac4a21146868a4bfbdd0af79
myizci <izci@hotmail.com>
2021-01-26 15:22:48 -0500

    movie1  more work updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     //This is  a demo file
     public static void main(String[] args) {
         System.out.println("Hold your horses young man! USA");
+        //watching more movie  somewhere else
 
     }
 }
